b2244b9d4efaa13316f8ab0bd490b94aa4329c03
kkh <k1k2k3kkh@gmail.com>
2024-01-09 15:27:32 +0900

    init

---
+++ ch02_pjt_01/src/ch02_pjt_01/MainClass.java
@@ -0,0 +1,14 @@
+package ch02_pjt_01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyCalculator calculator = new MyCalculator();
+		calculator.calAdd(10, 5);
+		calculator.calSub(10, 5);
+		calculator.calMul(10, 5);
+		calculator.calDiv(10, 5);
+	}
+
+}
